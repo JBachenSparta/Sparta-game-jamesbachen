022acacdde89499e736e8a916061a4a629d927a0
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2019-04-16 14:42:39 +0100

    Dealer hands out cards, dealt cards appear on table, dealer has been automated and will only bet when their hand is lessthan or equal to 16

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(function (){
 class Deck {
   constructor() {
     this.deck = [];
@@ -30,27 +31,8 @@ class Deck {
   }
 }
 
-// function split (test){
-  // var str = deck1.deal();
-  // var test = str.split(" ");
-  //dealt.push(cardData);
-  // test = cardData;
-//   return test = deck1.deal().split(" ");
-//
-// }
-
 
 function cardValues (completeSplitCard){
-// function cardValues (blackJackValue, faceValue, faceSuit){
-  // blackJackValue = "";
-  // faceValue = "";
-  // faceSuit = "";
-
-
-
-// function cardValues (bjValue, value, suit){
-  // var test = (split(test));
-  // console.log(test);
 
   splitDealtCard = deck1.deal().split(" ");
   var valuesFace = ["King", "Queen", "Jack"];
@@ -59,8 +41,6 @@ function cardValues (completeSplitCard){
   value = splitDealtCard[0];
   suit = splitDealtCard[2];
   completeSplitCard = [];
-
-  // console.log(value);
 
   if (valuesFace.indexOf(value) >= 0) {
     blackJackValue = 10;
@@ -82,29 +62,127 @@ function cardValues (completeSplitCard){
   }
 
   completeSplitCard.push(suit)
-  console.log(completeSplitCard);
 
   return completeSplitCard;
-  // console.log(suit)
-  // return suit
+
 }
 
 function userCards () {
   var completeSplitCard = [];
+
   completeSplitCard = cardValues (completeSplitCard);
-  console.log(completeSplitCard)
-// function userCards (blackJackValue, faceValue, faceSuit) {
-  // var blackJackValue = cardValues(blackJackValue);
-  // var faceValue = cardValues(faceValue);
-  // var faceSuit = cardValues(faceSuit);
-  // console.log(blackJackValue, faceValue, faceSuit);
+  dealtUserCards.push(completeSplitCard);
+  blackJackUserTotal +=  parseInt(completeSplitCard[0]);
+
+  //
+  // for (var i = 0; i < completeSplitCard.length; i++) {
+  //   for (var j = 0; j < completeSplitCard.length; j++) {
+  //     completeSplitCard[j]
+  //   }
+  //   completeSplitCard[i][j]
+  //   console.log(completeSplitCard[i].length);
+  //
+  // }
+  console.log(completeSplitCard[2]);
+
+
+//$('<img src="images/' + completeSplitCard[1] + completeSplitCard[2] + '.png">').appendTo(".userCardSpace" + hitCount );
 
 }
 
+function dealerCards () {
+  var completeSplitCard = [];
+  completeSplitCard = cardValues (completeSplitCard);
+  dealtDealerCards.push(completeSplitCard);
+  blackJackDealerTotal += parseInt(completeSplitCard[0]);
+
+  console.log(completeSplitCard[2]);
+  console.log(blackJackDealerTotal);
+}
+
+
+
+
+function automateDealer () {
+  while (blackJackDealerTotal <= 16){
+    dealerCards();
+
+  }
+
+  if (blackJackDealerTotal == 21) {
+    console.log("Black Jack");
+  }
+
+  else if (blackJackDealerTotal < 21 && blackJackDealerTotal > 16) {
+    console.log("Dealer Stays");
+      // console.log(blackJackDealerTotal);
+  }
+  else  {
+    console.log("Dealer is bust");
+      // console.log(blackJackDealerTotal);
+  }
+
+}
+
+
 var deck1 = new Deck();
+var dealtUserCards = [];
+var dealtDealerCards = [];
+var hitCount = 0;
+var blackJackUserTotal = 0;
+var blackJackDealerTotal = 0
 
 deck1.shuffle();
 console.log(deck1.deck);
-userCards ();
+console.log(blackJackUserTotal);
 
-console.log(deck1.deck);
+// $('.hitButton').html(function(i, userCardSpace) { return +userCardSpace+1 });
+
+//console.log(dealtUserCards[hitCount][1]);
+
+$(".hitButton").on("click", function (){
+
+  userCards();
+  console.log(blackJackUserTotal);
+  $('<img src="images/' + dealtUserCards[hitCount][1] + dealtUserCards[hitCount][2] + '.png">').appendTo(".userCardSpace"+hitCount);
+
+  //$('userCardSpace'+hitcount)
+  //if statment if value of cards it greater than 21 go bust, play dealer after interval.
+  hitCount++;
+
+  if (blackJackUserTotal == 21) {
+    console.log("BlackJack");
+  }
+
+  else if (blackJackUserTotal > 21){
+    console.log("Bust");
+    automateDealer();
+
+  }
+  else {
+    console.log("Play on ");
+  }
+
+});
+
+$(".dealerButton").on("click", function (){
+  dealerCards();
+});
+
+
+$(".stayButton").on("click", function (){
+  automateDealer();
+});
+
+
+
+
+
+
+
+  // $(".hitButton").on("click", function (){
+  //   userCards();
+  //   console.log(dealtUserCards);
+
+
+  })
